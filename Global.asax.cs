16877f2b9f827ec036d5142160ad6f2be3f98a31
shkalyan <44196358+shkalyan@users.noreply.github.com>
2019-08-03 21:43:47 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Data.Entity;
+using WingtipToys.Models;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Database.SetInitializer(new ProductDatabaseInitializer());
+            RoleActions roleActions = new RoleActions();
+            roleActions.createAdmin();
+            RegisterCustomRoutes(RouteTable.Routes);
+        }
+        void RegisterCustomRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute(
+                "ProductsByCategoryRoute",
+                "Category/{categoryName}",
+                "~/ProductList.aspx"
+            );
+            routes.MapPageRoute(
+                "ProductByNameRoute",
+                "Product/{productName}",
+                "~/ProductDetails.aspx"
+            );
+        }
+    }
+}
